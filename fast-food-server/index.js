4f717772b82cf5d2fa54f1b46f40d99dba44e4ea
Md Moniruzzaman Lelin <lelin171046@gmail.com>
2025-06-21 19:16:16 +0600

    authentication setup

--- fast-food-server/index.js
+++ fast-food-server/index.js
@@ -18,9 +18,9 @@ const corsOption = {
   app.use(cors(corsOption));
   app.use(express.json());
 
-// const uri = `mongodb://localhost:27017/`
+const uri = `mongodb://localhost:27017/`
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0f5vnoo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0f5vnoo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
 const client = new MongoClient(uri, {
